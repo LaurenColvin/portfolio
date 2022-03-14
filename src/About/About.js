2216b5a810aef96bb3e7ee05b5860704978153e4
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-13 23:01:56 -0700

    adding icons

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,12 @@
-const About = () => {
+const About = (props) => {
     return (
         <div>
             <h1>About Me</h1>
+            {props.htmlIcon}
+            {props.cssIcon}
+            {props.jsIcon}
+            {props.reactIcon}
+            {props.githubIcon}
         </div>
     )
 }
