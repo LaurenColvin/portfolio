e3d87c936a49577c804a0933d7607b1c63debf51
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-13 17:14:13 -0700

    creating React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Routes, Link, Route} from "react-router-dom"
+import Home from "./Home/Home";
+import Work from "./Work/Work";
+import About from "./About/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="logo">
+          <Link className="link" to="/"><h2>LC</h2></Link>
+        </div>
+        <div className="routes">
+          <Link className="link" to="/work"><h3>work</h3></Link>
+          <Link className="link" to="/about"><h3>about</h3></Link>
+        </div>
+      </nav>
+      <div className="main">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/work" element={<Work />}/>
+            <Route path="/about" element={<About />}/>
+        </Routes>
+      </div>
+      <footer>
+        <h5>Created and Designed by Lauren Colvin</h5>
+      </footer>
     </div>
   );
 }
