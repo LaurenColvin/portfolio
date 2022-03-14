39fe8f3621d468412b7cf0a8ca36fa26a90e887b
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-13 23:03:05 -0700

    importing font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
 import {Routes, Link, Route} from "react-router-dom"
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {faHtml5} from '@fortawesome/free-brands-svg-icons'
+import {faCss3Alt} from '@fortawesome/free-brands-svg-icons'
+import {faReact} from '@fortawesome/free-brands-svg-icons'
+import {faJsSquare} from '@fortawesome/free-brands-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+
 import Home from "./Home/Home";
 import Work from "./Work/Work";
 import About from "./About/About";
 
+let menu = <FontAwesomeIcon icon={faBars} size="2x" style={{color: '#AB4289'}}/>
+let htmlIcon = <FontAwesomeIcon icon={faHtml5} size="2x" style={{color: '#AB4289'}}/>
+let cssIcon = <FontAwesomeIcon icon={faCss3Alt} size="2x" style={{color: '#AB4289'}}/>
+let reactIcon = <FontAwesomeIcon icon={faReact} size="2x" style={{color: '#AB4289'}}/>
+let jsIcon = <FontAwesomeIcon icon={faJsSquare} size="2x" style={{color: '#AB4289'}}/>
+let githubIcon = <FontAwesomeIcon icon={faGithub} size="2x" style={{color: '#AB4289'}}/>
+
+
+
 function App() {
+
+const [menuOpen, setMenuOpen] = useState(false)
+
+const handleToggle = () => {
+  setMenuOpen(prev => !prev)
+}
+
   return (
     <div className="App">
       <nav>
@@ -15,12 +40,25 @@ function App() {
           <Link className="link" to="/work"><h3>work</h3></Link>
           <Link className="link" to="/about"><h3>about</h3></Link>
         </div>
+        {menuOpen === false ? (
+          <div className="menu-closed" onClick={handleToggle}>
+            {menu}
+          </div>
+        ) : (
+          <div className="menu-open">
+            <div className="menu-closed" onClick={handleToggle}>
+              {menu}
+            </div>
+            <Link  onClick={handleToggle} className="link" to="/work"><h3>work</h3></Link>
+            <Link  onClick={handleToggle} className="link" to="/about"><h3>about</h3></Link>
+          </div>
+        )}
       </nav>
       <div className="main">
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/work" element={<Work />}/>
-            <Route path="/about" element={<About />}/>
+            <Route path="/about" element={<About githubIcon={githubIcon} jsIcon={jsIcon} reactIcon={reactIcon} cssIcon={cssIcon} htmlIcon={htmlIcon} />}/>
         </Routes>
       </div>
       <footer>
