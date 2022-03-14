400f93d04c39313816ecf93b3e80218365df9072
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-13 17:13:25 -0700

    creating home page for react app

---
+++ src/Home/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+    return (
+        <div className="home-grid">
+            <div className="left-column">
+                <h1 className="blurb">
+                    Lauren Colvin is a designer turned software engineer living in Los
+                    Angeles California.
+                </h1>
+            </div>
+            <div className="right-column">
+            </div>
+            <div className="center">
+            <a className="link" id="button" href='https://drive.google.com/file/d/1v4_cmWjYucDVhR3n9FIy0Fe8HgadRpDt/view?usp=sharing' target="_blank">Resume</a>
+            <a className="link" id="button" href='https://www.linkedin.com/in/laurenleighcolvin/' target="_blank">Linkedin</a>
+            </div> 
+        </div>
+    )
+}
+
+export default Home
